b04a59427097ddf1e983c848ad092a8e1e3cbdf3
azicha <askakauchi@gmail.com>
2023-11-04 23:07:23 +0300

    Initial commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,37 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Bata Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/bata.png',
+    'company': 'Bata',
+  },
+  {
+    'id': '1',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/nike.png',
+    'company': 'Nike',
+  },
+  {
+    'id': '2',
+    'title': 'Men\'s Addidas Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/addidas.png',
+    'company': 'Addidas',
+  },
+];
+
+final cart = [
+  {
+    'id': '3',
+    'title': 'Men\'s Addidas Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/addidas.png',
+    'company': 'Addidas',
+  }
+];
---
+++ lib/product_details_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+  final Map<String, Object> product;
+  const ProductDetailsPage({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Details'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            product['title'] as String,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset(product['imageUrl'] as String),
+          ),
+          const Spacer(flex: 2),
+          Container(
+            height: 150,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  '\$${product['price']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 10),
+                ListView.builder(itemBuilder: itemBuilder)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
